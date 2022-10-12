e07d81dbc26de61a97ef8d7fca1e80cc91d5927f
waonderboy <crazy7523@gmail.com>
2022-10-12 18:59:19 +0900

    #7 - 게시글 상세페이지 틀 작성
    게시글 상세페이지를 위해 뷰, 컨트롤러, 테스트를 작성하였다

--- src/main/java/com/personal/projectboard/controller/ArticleController.java
+++ src/main/java/com/personal/projectboard/controller/ArticleController.java
@@ -2,9 +2,9 @@ package com.personal.projectboard.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,6 +17,13 @@ public class ArticleController {
     public String articles(ModelMap map){
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map){
+        map.addAttribute("article", "article");
+        map.addAttribute("articleComments", "articleComment");
+        return "articles/details";
     }
 
 }
--- src/test/java/com/personal/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/personal/projectboard/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.personal.projectboard.controller;
 
+import com.personal.projectboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,6 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
@@ -38,7 +41,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles")); // 키가 있는지 체크
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
@@ -49,7 +51,7 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/details"))
-                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
     }
 
