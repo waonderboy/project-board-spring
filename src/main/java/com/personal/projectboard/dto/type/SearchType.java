445eb3a0df5b6af8e704853d1c33719f7ca7ae90
waonderboy <crazy7523@gmail.com>
2022-10-13 14:59:38 +0900

    #19 - 게시글 CRUD 테스트 개발
    게시글 CRUD 기능을 개발하였다. read와 update시 필요한 dto 클래스를 작성하고 이에 해당하는 테스트도 작성하였다.

---
+++ src/main/java/com/personal/projectboard/dto/type/SearchType.java
@@ -0,0 +1,5 @@
+package com.personal.projectboard.dto.type;
+
+public enum SearchType {
+    TITLE, ID, NICKNAME, HASHTAG, CONTENT
+}
---
+++ src/main/java/com/personal/projectboard/service/ArticleService.java
@@ -0,0 +1,43 @@
+package com.personal.projectboard.service;
+
+
+import com.personal.projectboard.dto.ArticleDto;
+import com.personal.projectboard.dto.ArticleUpdateDto;
+import com.personal.projectboard.dto.type.SearchType;
+import com.personal.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long articleId) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+
+    }
+
+    public void deleteArticle(long articleId) {
+
+    }
+}
