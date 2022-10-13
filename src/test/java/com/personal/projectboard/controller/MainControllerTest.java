3ac43d39549c337d123b8037bbb678a17075f681
waonderboy <crazy7523@gmail.com>
2022-10-13 14:56:46 +0900

    #19 - 루트페이지 테스트 작성 및 개발
    루트페이지 진입시 게시판 페이지로 리다이렉션

---
+++ src/test/java/com/personal/projectboard/controller/MainControllerTest.java
@@ -0,0 +1,34 @@
+package com.personal.projectboard.controller;
+
+import com.personal.projectboard.config.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Import(SecurityConfig.class)
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void givenNothing_whenRequestingRootPage_thenRedirectsToArticlePage() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection());
+
+
+    }
+
+}
