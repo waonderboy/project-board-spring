d502af5741fc56451dd0a7137ccc511ddaa896c3
waonderboy <crazy7523@gmail.com>
2022-10-16 00:15:05 +0900

    #19 - 게시판 서비스 검색기능 쿼리를 개발
    Querydsl을 활용하여 게시판 서비스 검색기능 쿼리를 개발하였다. 검색 타입에 맞는 키워드로 게시글을 검색할 수 있도록 하였고, 타입이 없을 시 전체 게시글이 검색되도록 하였다.
    Querydsl을 활용해 동적쿼리로 페이징 기능을 추가하여 개발하였다.

--- src/main/java/com/personal/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/personal/projectboard/repository/ArticleRepository.java
@@ -14,6 +14,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
+        ArticleRepositoryQuerydsl,
         QuerydslPredicateExecutor<Article>,
         QuerydslBinderCustomizer<QArticle> {
 
