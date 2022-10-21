43f85a3af5b48102abff9596e3997ea99a2f4925
waonderboy <crazy7523@gmail.com>
2022-10-21 16:16:11 +0900

    #38 - 댓글 등록폼 구현

--- src/main/java/com/personal/projectboard/dto/request/ArticleCommentRequest.java
+++ src/main/java/com/personal/projectboard/dto/request/ArticleCommentRequest.java
@@ -1,4 +1,20 @@
 package com.personal.projectboard.dto.request;
 
-public class ArticleCommentRequest {
+import com.personal.projectboard.dto.ArticleCommentDto;
+import com.personal.projectboard.dto.UserAccountDto;
+
+public record ArticleCommentRequest(Long articleId, String content) {
+
+    public static ArticleCommentRequest of(Long articleId, String content) {
+        return new ArticleCommentRequest(articleId, content);
+    }
+
+    public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
+        return ArticleCommentDto.of(
+                articleId,
+                userAccountDto,
+                content
+        );
+    }
+
 }
