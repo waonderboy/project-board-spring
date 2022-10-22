259d896d08b3e021734e93321de7cbcc164be6e4
waonderboy <crazy7523@gmail.com>
2022-10-22 17:07:08 +0900

    #40 - 인증을 JPA Auditing에 적용

--- src/main/java/com/personal/projectboard/config/JpaConfig.java
+++ src/main/java/com/personal/projectboard/config/JpaConfig.java
@@ -1,18 +1,29 @@
 package com.personal.projectboard.config;
 
+import com.personal.projectboard.dto.security.BoardPrincipal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.security.Principal;
 import java.util.Optional;
 
 @EnableJpaAuditing
 @Configuration
 public class JpaConfig {
-
+    //CreatedBy 누가 만들었는지에 대한 정보
     @Bean
-    public AuditorAware<String>auditorAware() {
-        return () -> Optional.of("kim"); // TODO : 스프 시큐리티로 인증기능을 붙힐때, 수정 필요
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.ofNullable(SecurityContextHolder
+                .getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(BoardPrincipal.class::cast)
+                .map(BoardPrincipal::getUsername);
     }
 }
