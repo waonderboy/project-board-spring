d72954534b8df141f8d8dc3a96be567c4b7058ef
waonderboy <crazy7523@gmail.com>
2022-10-22 17:05:45 +0900

    #40 - 인증서비스 구현
    게시판을 이용하기 위한 Principal과 관련된 정보를 구현함.
    Principal를 발행하기 위한 userDetailService와
    인증을 적용할 SecurityFilterChain을 구현하였다.
    또한 passwordEncoder를 설정하였다.

--- src/main/java/com/personal/projectboard/config/SecurityConfig.java
+++ src/main/java/com/personal/projectboard/config/SecurityConfig.java
@@ -1,21 +1,67 @@
 package com.personal.projectboard.config;
 
+import com.personal.projectboard.dto.UserAccountDto;
+import com.personal.projectboard.dto.security.BoardPrincipal;
+import com.personal.projectboard.repository.UserAccountRepository;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 // @EnableWebSecurity 안써도 됨 스프링 부트에서 스프링 시큐리티를 연동할 때 쓰느것
 @Configuration
 public class SecurityConfig {
 
+    /*
+    스프링 시큐리티 관리하에 인증 인가 설정
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeRequests(auth -> auth.anyRequest().permitAll())
+                .authorizeRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .mvcMatchers(
+                                HttpMethod.GET,
+                                "/",
+                                "/articles",
+                                "/articles/search-hashtag"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
                 .formLogin().and()
+                .logout()
+                    .logoutSuccessUrl("/")
+                    .and()
                 .build();
+    }
 
+    /*
+    시큐리티 자체를 적용할지 말지
+    시큐리티 적용 제외 static resource, css, js
+     */
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        return username -> userAccountRepository.findById(username)
+                .map(UserAccountDto::from)
+                .map(BoardPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다. - username" + username));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
